8d3f3df0da88be41617afcaee053197e32ead8d7
everton <evertonmusiyiwa@gmail.com>
2026-02-07 13:44:17 +0200

    Initial commit including GitHub Actions workflow

---
+++ main.js
@@ -0,0 +1,112 @@
+const { app, BrowserWindow, dialog } = require('electron');
+const path = require('path');
+const { autoUpdater } = require('electron-updater');
+const log = require('electron-log');
+
+// Configure logging
+autoUpdater.logger = log;
+autoUpdater.logger.transports.file.level = 'info';
+
+let mainWindow;
+let splashWindow;
+
+function createSplashWindow() {
+    splashWindow = new BrowserWindow({
+        width: 500,
+        height: 300,
+        frame: false,
+        transparent: true,
+        alwaysOnTop: true,
+        center: true,
+        webPreferences: {
+            nodeIntegration: false
+        }
+    });
+
+    splashWindow.loadFile('splash.html');
+}
+
+function createMainWindow() {
+    mainWindow = new BrowserWindow({
+        width: 1280,
+        height: 800,
+        show: false, // Initially hide while loading
+        autoHideMenuBar: true, // Native look: Hide menus
+        icon: path.join(__dirname, 'assets/icon.ico'),
+        webPreferences: {
+            nodeIntegration: false,
+            contextIsolation: true,
+            devTools: false // Production grade: Disable DevTools
+        }
+    });
+
+    // Load the Vercel URL
+    mainWindow.loadURL('https://puritytrading.vercel.app');
+
+    // Once the main system is ready, switch windows
+    mainWindow.once('ready-to-show', () => {
+        if (splashWindow) {
+            splashWindow.destroy();
+        }
+        mainWindow.show();
+        mainWindow.maximize();
+    });
+
+    // Handle crash or refresh
+    mainWindow.on('closed', () => {
+        mainWindow = null;
+    });
+}
+
+app.on('ready', () => {
+    createSplashWindow();
+    createMainWindow();
+
+    // Check for updates immediately
+    autoUpdater.checkForUpdatesAndNotify();
+});
+
+app.on('window-all-closed', () => {
+    if (process.platform !== 'darwin') {
+        app.quit();
+    }
+});
+
+app.on('activate', () => {
+    if (mainWindow === null) {
+        createMainWindow();
+    }
+});
+
+// Auto-Updater Events
+autoUpdater.on('checking-for-update', () => {
+    log.info('Checking for update...');
+});
+autoUpdater.on('update-available', (info) => {
+    log.info('Update available.');
+    // Automatically download
+    autoUpdater.downloadUpdate();
+});
+autoUpdater.on('update-not-available', (info) => {
+    log.info('Update not available.');
+});
+autoUpdater.on('error', (err) => {
+    log.info('Error in auto-updater. ' + err);
+});
+autoUpdater.on('download-progress', (progressObj) => {
+    let log_message = "Download speed: " + progressObj.bytesPerSecond;
+    log_message = log_message + ' - Downloaded ' + progressObj.percent + '%';
+    log_message = log_message + ' (' + progressObj.transferred + "/" + progressObj.total + ')';
+    log.info(log_message);
+});
+autoUpdater.on('update-downloaded', (info) => {
+    log.info('Update downloaded');
+    dialog.showMessageBox({
+        type: 'info',
+        title: 'Update Ready',
+        message: 'A new version has been downloaded. Restart the application to apply the updates.',
+        buttons: ['Restart & Install']
+    }).then((returnValue) => {
+        if (returnValue.response === 0) autoUpdater.quitAndInstall();
+    });
+});
